b46ca8f5e146d04f88dc61fc7538b629afb18da6
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2022-05-02 00:08:46 +0530

    FIX: added var to colors

--- src/pages/Notes-Page/Filter/Filter.jsx
+++ src/pages/Notes-Page/Filter/Filter.jsx
@@ -8,12 +8,12 @@ const Filter = () => {
         BY DATE:
       </label>
       <li className="list-item filter-item">
-        <label className="filter-item-title old-date">
-          <input className="filter-radio" type="radio" name="sort" />
+        <label className="filter-item-title">
+          <input className="filter-radio" type="radio" name="date" />
           <p className="para-xsmall">Oldest to Newest</p>
         </label>
-        <label className="filter-item-title new-date">
-          <input className="filter-radio" type="radio" name="sort" />
+        <label className="filter-item-title">
+          <input className="filter-radio" type="radio" name="date" />
           <p className="para-xsmall">Newest to Oldest</p>
         </label>
       </li>
@@ -25,38 +25,42 @@ const Filter = () => {
       </label>
 
       <li className="list-item filter-item">
-        <label className="filter-item-title tag-work">
-          <input className="filter-radio" type="radio" name="sort" />
-          <p className="para-xsmall">Work</p>
-        </label>
-        <label className="filter-item-title tag-exercise">
-          <input className="filter-radio" type="radio" name="sort" />
-          <p className="para-xsmall">Exercise</p>
-        </label>
-        <label className="filter-item-title tag-health">
-          <input className="filter-radio" type="radio" name="sort" />
-          <p className="para-xsmall">Health</p>
-        </label>
-        <label className="filter-item-title tag-chores">
-          <input className="filter-radio" type="radio" name="sort" />
-          <p className="para-xsmall">Chores</p>
-        </label>
+        <div className="two-items">
+          <label className="filter-item-title">
+            <input className="filter-radio" type="radio" name="tag" />
+            <p className="para-xsmall">Work</p>
+          </label>
+          <label className="filter-item-title">
+            <input className="filter-radio" type="radio" name="tag" />
+            <p className="para-xsmall">Exercise</p>
+          </label>
+        </div>
+        <div className="two-items">
+          <label className="filter-item-title">
+            <input className="filter-radio" type="radio" name="tag" />
+            <p className="para-xsmall">Health</p>
+          </label>
+          <label className="filter-item-title">
+            <input className="filter-radio" type="radio" name="tag" />
+            <p className="para-xsmall">Chores</p>
+          </label>
+        </div>
       </li>
 
       <div className="filter-divider"></div>
 
       <li className="filter-section-title">BY PRIORITIES:</li>
       <li className="list-item filter-item">
-        <label className="filter-item-title prio-low">
-          <input className="filter-radio" type="radio" name="sort" />
+        <label className="filter-item-title">
+          <input className="filter-radio" type="radio" name="priority" />
           <p className="para-xsmall">Low</p>
         </label>
-        <label className="filter-item-title prio-medium">
-          <input className="filter-radio" type="radio" name="sort" />
+        <label className="filter-item-title">
+          <input className="filter-radio" type="radio" name="priority" />
           <p className="para-xsmall">Medium</p>
         </label>
-        <label className="filter-item-title prio-high">
-          <input className="filter-radio" type="radio" name="sort" />
+        <label className="filter-item-title">
+          <input className="filter-radio" type="radio" name="priority" />
           <p className="para-xsmall">High</p>
         </label>
       </li>
